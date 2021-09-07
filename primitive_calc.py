46211ffb1ddf93d40d1705aea9d417e15cbfb8e2
hellannie <73584580+hellannie@users.noreply.github.com>
2021-09-07 15:21:13 +0500

    Добавлены операции умножения и деления

--- primitive_calc.py
+++ primitive_calc.py
@@ -7,6 +7,10 @@ def substract(x,y):
     return x-y
 def averageModules(x,y):
     return (abs(x)+abs(y))/2
+def multiply(x,y):
+    return x*y
+def divide(x,y):
+    return x/y
 
 a, b = input("Введите два числа:").split()
 a = int(a)
@@ -14,4 +18,6 @@ b = int(b)
 print("Сумма:" , add(a,b))
 print("Разность:" , substract(a,b))
 print("Среднее арифметическое модулей: %.4f" %averageModules(a,b))
+print("Произведение:", multiply(a,b))
+print("Частное: %.4f" %  divide(a,b))
 m.getch()
